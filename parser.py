baee20348658182e34458929796626c0dd4b2be7
Juan <juanrosario38@gmail.com>
2019-12-27 00:47:42 +0000

    updated: .encode('utf-8')

--- parser.py
+++ parser.py
@@ -82,6 +82,6 @@ if __name__ == '__main__':
     text = parsed['content']
     doc = nlp(text)
     for ents in doc.ents:
-        print(ents.text+" ~ "+ents.label_)
+        print(str(ents.text.encode('utf-8')))
     #print (extract_name(nlp,text))
-    print(extract_clean_resume(text))
+    #print(extract_clean_resume(text))
