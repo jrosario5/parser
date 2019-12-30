be76d61915dd85b5ad67688f5ee437e876f1444e
Juan <juanrosario38@gmail.com>
2019-12-30 20:20:48 +0000

    Update parser.py

--- parser.py
+++ parser.py
@@ -94,5 +94,5 @@ if __name__ == '__main__':
     text = extract_clean_resume(parsed['content'])
     doc = nlp(text)
     # print(doc)
-    for ents in doc.ents:
-        print(str(ents.text) + " ~ " + str(ents.label_))
+    """for ents in doc.ents:
+        print(str(ents.text) + " ~ " + str(ents.label_))"""
