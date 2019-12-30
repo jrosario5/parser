af9d368b8501b547af7c956932a8efebcf876b33
root <bpeguero.wp@gmail.com>
2019-12-30 14:21:27 -0500

    updated parser

--- parser.py
+++ parser.py
@@ -4,14 +4,17 @@ import os
 import json
 import pandas as pd
 
+
 def extract_clean_resume(text):
-    splitByline     = text.split('\n')
-    i=0
-    resumeText=''
+    splitByline = text.split('\n')
+    i = 0
+    resumeText = ''
     for line in splitByline:
         if not re.match(r'^\s*$', line):
-            resumeText+=line +'\n'
+            resumeText += line + '\n'
     return resumeText
+
+
 def extract_phone_numbers(document):
     docSplit = document.split('\n')
     doc = ''
@@ -19,68 +22,77 @@ def extract_phone_numbers(document):
     for newDoc in docSplit:
         doc += newDoc+'\n'
         ing += 1
-        if(ing>=5):
-            break;
-    r = re.compile(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})')
+        if(ing >= 5):
+            break
+    r = re.compile(
+        r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})')
     phone_numbers = r.findall(doc)
     return [re.sub(r'\D', '', number) for number in phone_numbers]
+
 
 def extract_email_addresses(string):
     r = re.compile(r'[\w\.-]+@[\w\.-]+')
     return r.findall(string)
 
+
 def extract_name(n, document):
     docs = n(document)
     for entity in docs.ents:
-        if(entity.label_=='NAME'):
+        if(entity.label_ == 'NAME'):
             name = entity.text
-            break;
+            break
     return name
+
+
 def extract_skills(document):
     doc = n(document)
     skills = ''
     #df = pd.DataFrame()
     items = []
     for skill in doc.ents:
-        if(skill.label_=='SKILLS'):
-            #skills.append(skill.text)
-            items.append({'skills':skill.text})
+        if(skill.label_ == 'SKILLS'):
+            # skills.append(skill.text)
+            items.append({'skills': skill.text})
             #skills +=str(skill.text)+';'
     df = pd.DataFrame(items, columns=['skills'])
-    #print df.skills
-    dfd=df.drop_duplicates()
+    # print df.skills
+    dfd = df.drop_duplicates()
     for ind in dfd.index:
-        #print(dfd['skills'][ind])
-        skills +=str(dfd['skills'][ind])+';'
+        # print(dfd['skills'][ind])
+        skills += str(dfd['skills'][ind])+';'
     return str(skills)
+
 
 def extract_home_address(document):
     doc = n(document)
     h = ''
     for ent in doc.ents:
-        if ent.label_=='HOME_ADDRESS':
-            h=ent.text
-            break;
+        if ent.label_ == 'HOME_ADDRESS':
+            h = ent.text
+            break
     return h
+
+
 def extract_url(document):
-    doc= n(document)
-    u=''
+    doc = n(document)
+    u = ''
     for uent in doc.ents:
-        if uent.label_=='URL':
+        if uent.label_ == 'URL':
             u = uent.text
-            break;
+            break
     return u
+
 
 if __name__ == '__main__':
     nlp = spacy.load("gp")
-    
+
     import tika
     from tika import parser
-    parsed = parser.from_file('Srinivasan.pdf')
-    ### converting changes the format
-    #text = str(parsed["content"].encode('utf-8')) ### converting changes the format
-    text = parsed['content']
+    parsed = parser.from_file('resumeT1.pdf')
+    # converting changes the format
+    # text = str(parsed["content"].encode('utf-8')) ### converting changes the format
+    text = extract_clean_resume(parsed['content'])
     doc = nlp(text)
+    # print(doc)
     for ents in doc.ents:
-        print(str(ents.text.encode('utf-8')) +" ~ "+ str(ents.label_))
-        
+        print(str(ents.text) + " ~ " + str(ents.label_))
